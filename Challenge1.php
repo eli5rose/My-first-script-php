febdb672ad4201f1cb293e0e8e75bece8a51c565
eli5rose <eli5rose@yahoo.com>
2022-02-08 14:30:09 +0100

    Adding all my php files

---
+++ Challenge1.php
@@ -0,0 +1,17 @@
+<?php
+
+$bookName = "Voyage avec un âne dans les Cévennes";
+$bookRead = true;
+$bookPublished = 2017;
+$bookPrice = 6.00;
+
+echo "$bookName <br>";
+if ($bookRead) {
+    echo "I haven't read the book <br>";
+} else {
+    echo "I have read the book <br>";
+
+}
+    echo "$bookPublished <br>";
+    echo "$bookPrice <br>";
+?>
